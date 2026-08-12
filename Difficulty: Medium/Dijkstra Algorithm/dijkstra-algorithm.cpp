6661f2967bafc94e12ee5f49297bc70b229bb297
Souparno Nag <161619150+souparno-nag@users.noreply.github.com>
2026-08-12 11:45:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -2,28 +2,31 @@ class Solution {
   public:
     vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
         // prepare the adjacency list
-        vector<vector<pair<int, int>>> adjList(V);
+        vector<vector<pair<int, int>>> adjList(V+1);
         for (int i = 0; i < edges.size(); i++) {
             int u = edges[i][0], v = edges[i][1], w = edges[i][2];
             adjList[u].push_back({v, w});
             adjList[v].push_back({u, w});
         }
         // prepare data structures
-        priority_queue <pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        set<pair<int, int>> st;
         vector<int> dist(V, 1e9);
         // initialize
         dist[src] = 0;
-        pq.push({0, src});
+        st.insert({0, src});
         // calculate the distances
-        while (!pq.empty()) {
-            int dis = pq.top().first, node = pq.top().second;
-            pq.pop();
-            if (dis != dist[node]) continue;
+        while (!st.empty()) {
+            auto it = *(st.begin());
+            int dis = it.first, node = it.second;
+            st.erase(it);
             for (auto it: adjList[node]) {
                 int adjNode = it.first, edgeWeight = it.second;
                 if (dis + edgeWeight < dist[adjNode]) {
+                    if (dist[adjNode] != 1e9) {
+                        st.erase({dist[adjNode], adjNode});
+                    }
                     dist[adjNode] = dis + edgeWeight;
-                    pq.push({dist[adjNode], adjNode});
+                    st.insert({dist[adjNode], adjNode});
                 }
             }
         }
